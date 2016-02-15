d61ba03a96a11f20625afab4ceee705033f89086
g8y3e <pava@PC-pava.kyiv.ciklum.net>
2016-02-15 16:12:06 +0200

    added cloud shell api source

--- cloudshell/__init__.py
+++ cloudshell/__init__.py
@@ -1,1 +1,3 @@
-__author__ = 'ini'
+__author__ = 'g8y3e'
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)
---
+++ cloudshell/api/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'g8y3e'
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)
